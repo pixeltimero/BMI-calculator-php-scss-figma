84abcf7e33b66d0e3cfb9b14ce66c1c2e9633e55
Florin Matei <contact@florinmatei.com>
2020-03-25 00:03:32 +0200

    HTML boilerplate

---
+++ index.php
@@ -0,0 +1,58 @@
+<html>
+    <head>
+        <title>BMI Calculator</title>
+        <link rel="stylesheet" href="styles.css">
+    </head>
+    <body>
+    <div class="container hide">
+  <div class="header">
+    BMI Calculator
+  </div>
+  <div class="body">
+    <div class="choose">
+      <div class="item">
+        Male
+      </div>
+      <div class="item">
+        Female
+      </div>
+    </div>
+    <div class="inputs">
+      <div class="item">
+        <span>Height (in cm)</span>
+        <input type="text" name="height">
+      </div>
+      <div class="item">
+        <span>Weight (in kgs)</span>
+        <input type="text" name="weight">
+      </div>
+    </div>
+  </div>
+  <div class="footer">
+    <button class="hide">Submit</button>
+    <button>Recalculate BMI</button>
+  </div>
+</div>
+<div class="container">
+  <div class="header">
+    BMI Calculator
+  </div>
+  <div class="body">
+    <div class="results">
+      <span>Your BMI is</span>
+      <strong>21.4</strong>
+    </div>
+    <div class="infos">
+      <span>Underweight = <18.5</span>
+      <span>Normal weight = 18.5–24.9</span>
+      <span>Overweight = 25–29.9</span>
+      <span>Obesity = BMI of 30 or greater</span>
+    </div>
+  </div>
+  <div class="footer">
+    <button class="hide">Submit</button>
+    <button>Recalculate BMI</button>
+  </div>
+</div>
+    </body>
+</html>
